cb598c610af1ddcc1dc63b1ff10249a6586b6550
cullinank2 <cullinank2@mail.sacredheart.edu>
2026-02-12 17:36:46 -0500

    kjc edit quad peer st.markdown

--- stockapp/app.py
+++ stockapp/app.py
@@ -651,7 +651,7 @@ def render_visualizations(
         # Executive Summary Text Block
         # ---------------------------------------------------------
         st.markdown(f"""
-        #### 📌 Quadrant Overview: **{quadrant_label} – {quadrant_name}**
+        #### 📌 Quadrant Overview: **{quadrant_label} – {quadrant_name}**{selected_ticker}** is positioned in this quadrant based on **PC1 ({PC1_INTERPRETATION['name']})** and **PC2 ({PC2_INTERPRETATION['name']})**
 
         _Interpretation_: {quadrant_desc}
 
