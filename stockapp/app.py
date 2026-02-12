2c803b19d4f303b9b049dff78a164a63c2eb975f
cullinank2 <cullinank2@mail.sacredheart.edu>
2026-02-12 17:30:05 -0500

    kjc edit quad peer st.markdown

--- stockapp/app.py
+++ stockapp/app.py
@@ -654,6 +654,7 @@ def render_visualizations(
         #### 📌 Quadrant Overview: **{quadrant_label} – {quadrant_name}**
 
         _Interpretation_: {quadrant_desc}
+
         ---
         **Peer Count:** {len(filtered_quadrant_peers)} comparable stocks 
 
